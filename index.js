8a7e40617d0b72661208a0c72b04d51c92716cfc
Cheryl <cheryl.dsouza@circleblack.com>
2025-05-17 19:50:49 +0530

    init

---
+++ index.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const cors = require("cors");
+const connectDB = require("./config/db");
+require("dotenv").config();
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+connectDB();
+
+app.use("/api/login", require("./routes/auth"));
+app.use("/api/messages", require("./routes/chat"));
+
+app.listen(process.env.PORT, () => {
+  console.log(`Server running on port ${process.env.PORT}`);
+});
+
+require("./websocket");
