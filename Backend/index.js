3d481bed90a01e651e10494f64ca362afe1da104
Cheryl <cheryl.dsouza@circleblack.com>
2025-05-18 16:54:13 +0530

    cors fixed

--- Backend/index.js
+++ Backend/index.js
@@ -9,7 +9,12 @@ require("dotenv").config();
 const app = express();
 const server = http.createServer(app);
 
-app.use(cors());
+app.use(cors({
+  origin: "*",
+  methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+  allowedHeaders: ["Content-Type", "Authorization"]
+}));
+
 app.use(express.json());
 
 connectDB();
