84d921bd7cde8e3871468fccf6218eb0d79c74f6
Cheryl <cheryl.dsouza@circleblack.com>
2025-05-18 16:43:23 +0530

    render changes for wss compatibility

--- Frontend/src/components/Login.js
+++ Frontend/src/components/Login.js
@@ -7,6 +7,7 @@ export default function Login({ onLoginSuccess, setUser }) {
   const [password, setPassword] = useState("");
 
   const handleLogin = async () => {
+    console.log(process.env)
     if (!email || !password) return;
 
     try {
